0f39f2a237e9f8dd7168b92165d896d586faad70
fuku-000 <s1F102201188@iniad.org>
2024-07-10 15:45:15 +0900

    fileの作成

---
+++ lib/fuku.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class FukuPage extends StatelessWidget {
+  const FukuPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> names = ['落合福', 'Jane Smith', 'Alice Johnson', 'Bob Brown','福澤裕生'];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fuku Page'),
+      ),
+      body: ListView.builder(
+        itemCount: names.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(names[index]),
+          );
+        },
+      ),
+    );
+  }
+}
