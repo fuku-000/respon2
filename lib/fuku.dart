83cfaf553e3b0b1faad279da39871ca43410cdcd
fuku-000 <s1F102201188@iniad.org>
2024-09-24 11:40:20 +0900

    スタンプ機能

--- lib/fuku.dart
+++ lib/fuku.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-class FukuPage extends StatelessWidget {
+class FukuPage extends StatefulWidget {
   const FukuPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final List<String> names = ['落合福', 'Jane Smith', 'Alice Johnson', 'Bob Brown','福澤裕生'];
+  _FukuPageState createState() => _FukuPageState();
+}
 
+class _FukuPageState extends State<FukuPage> {
+  final List<String> names = ['落合福', 'Jane Smith', 'Alice Johnson', 'Bob Brown', '福澤裕生'];
+  
+  // 各リスト項目にスタンプが押されているかどうかを管理するリスト
+  final List<bool> _isStamped = [false, false, false, false, false];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('スタンプカード'),
@@ -16,6 +24,15 @@ class FukuPage extends StatelessWidget {
         itemBuilder: (context, index) {
           return ListTile(
             title: Text(names[index]),
+            trailing: _isStamped[index] 
+              ? const Icon(Icons.check_circle, color: Colors.green)
+              : const Icon(Icons.radio_button_unchecked),
+            onTap: () {
+              setState(() {
+                // 押された項目のスタンプ状態をトグルする
+                _isStamped[index] = !_isStamped[index];
+              });
+            },
           );
         },
       ),
