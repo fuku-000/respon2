45e3fc580a0e0e1b91034bf45c048068684f0354
fuku-000 <s1F102201188@iniad.org>
2024-09-25 12:03:39 +0900

    スタンプ動きつき

--- lib/fuku.dart
+++ lib/fuku.dart
@@ -7,15 +7,15 @@ class FukuPage extends StatefulWidget {
   _FukuPageState createState() => _FukuPageState();
 }
 
-class _FukuPageState extends State<FukuPage> {
+class _FukuPageState extends State<FukuPage> with SingleTickerProviderStateMixin {
   // スタンプが押されているかどうかを管理するリスト
   final List<bool> _isStamped = List.generate(20, (index) => false);
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('すごろくスタンプカード'),
+        title: const Text('ポップなスタンプカード'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
@@ -34,19 +34,33 @@ class _FukuPageState extends State<FukuPage> {
                   _isStamped[index] = !_isStamped[index];
                 });
               },
-              child: Container(
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300), // アニメーションの長さ
+                curve: Curves.easeInOut, // スムーズなアニメーション
                 decoration: BoxDecoration(
-                  color: _isStamped[index] ? Colors.green : Colors.grey[300],
-                  border: Border.all(color: Colors.black),
-                  borderRadius: BorderRadius.circular(8),
+                  color: _isStamped[index]
+                      ? Colors.pinkAccent.withOpacity(0.8) // 押された時はポップなピンク色
+                      : Colors.lightBlueAccent.withOpacity(0.6), // 未押時は明るいブルー
+                  border: Border.all(color: Colors.black, width: 2.0),
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                      offset: const Offset(2, 2), // 影を少しだけ追加
+                    )
+                  ],
                 ),
                 child: Center(
                   child: _isStamped[index]
-                      ? const Icon(Icons.check, color: Colors.white, size: 30)
+                      ? const Icon(Icons.star, color: Colors.white, size: 36) // スタンプ後は星マーク
                       : Text(
                           '${index + 1}', // マスの番号を表示
                           style: const TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold),
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
                         ),
                 ),
               ),
